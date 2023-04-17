ba7b6988838f5376f353eccb4d9cb7b5dab0809a
wyswill <www.1151373571@live.com>
2023-04-17 18:50:49 +0800

    feat: type add

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ struct ProjectInfo {
 }
 
 fn main() {
-    let _config_path = "/Users/wangyansong/Library/Application Support/Code/User/globalStorage/alefragnani.project-manager/projects.json";
-    let vw_dir = "/Users/wangyansong/project/vw";
-    let cooper_dir = "/Users/wangyansong/project/cooper";
+    let _config_path: &str = "/Users/wangyansong/Library/Application Support/Code/User/globalStorage/alefragnani.project-manager/projects.json";
+    let vw_dir: &str = "/Users/wangyansong/project/vw";
+    let cooper_dir: &str = "/Users/wangyansong/project/cooper";
     let mut info_list: Vec<ProjectInfo> = Vec::new();
     scan_dir(cooper_dir, "cooper", info_list.borrow_mut());
     scan_dir(vw_dir, "vw", info_list.borrow_mut());
     println!("{:#?}", info_list);
-    let json_string = serde_json::to_string(&info_list).unwrap();
+    let json_string: String = serde_json::to_string(&info_list).unwrap();
     fs::write(_config_path, json_string).expect("写入失败");
     println!("写入成功");
 }
@@ -26,14 +26,14 @@ fn main() {
 fn scan_dir(_path: &str, tag: &str, info_list: &mut Vec<ProjectInfo>) {
     if let Ok(dir) = fs::read_dir(_path) {
         for entry in dir {
-            let _dir = entry.unwrap();
-            let path = _dir.path().to_str().unwrap().to_string();
+            let _dir: fs::DirEntry = entry.unwrap();
+            let path: String = _dir.path().to_str().unwrap().to_string();
             if path.contains(".DS_Store") {
                 continue;
             }
-            let name = path.replace(_path, "").replace("/", "");
-            let tags = vec![tag.to_string()];
-            let info = ProjectInfo {
+            let name: String = path.replace(_path, "").replace("/", "");
+            let tags: Vec<String> = vec![tag.to_string()];
+            let info: ProjectInfo = ProjectInfo {
                 name,
                 rootPath: path,
                 paths: Vec::new(),
