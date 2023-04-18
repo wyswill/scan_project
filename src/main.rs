4e36090ebbc64243566634c32253eb0f77689330
wyswill <www.1151373571@live.com>
2023-04-18 18:21:20 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::{borrow::BorrowMut, collections::HashSet, fs};
+use std::{collections::HashSet, fs};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ProjectInfo {
