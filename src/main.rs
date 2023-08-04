17b2a78ab00c553065088c4e09fe58238ab49786
wyswill <26476386+wyswill@users.noreply.github.com>
2023-08-04 15:57:51 +0800

    feat: fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use project_info::FileManager;
 use std::env;
 mod config;
 mod project_info;
@@ -5,7 +6,7 @@ mod project_info;
 fn main() {
     let _config_path: String = get_config_path();
     let _conf_ins: config::Config = config::Config::init_form_json(&_config_path);
-    let mut _project_manager: project_info::FileManager = project_info::FileManager::new(_conf_ins);
+    let mut _project_manager: FileManager = FileManager::new(_conf_ins);
     _project_manager.scan_dir();
     _project_manager.fmt();
     _project_manager.write_config();
